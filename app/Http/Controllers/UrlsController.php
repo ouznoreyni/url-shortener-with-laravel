8cdc4de3c63c0559595a4e1d8753ac7704f8abf6
ouznoreyni <ousmanediopp268@gmail.com>
2020-05-21 15:21:33 +0000

    modify our validation url

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -14,26 +14,28 @@ class UrlsController extends Controller
         return view('welcome');
     }
 
-    public function store()
+    public function store(Request $request)
     {
         $url = request()->get('url');
 
         //validation
-        Validator::make(
-            compact('url'),
-            [
-                'url' => ['required', 'url']
-            ]
-            //message error
-            //, [
-            //     'url.required' => "Vous devez fournir une URL.",
-            //     'url.url' => "Url est invalide."
+        //1
+        ///
+        //message error
+        //, [
+        //     'url.required' => "Vous devez fournir une URL.",
+        //     'url.url' => "Url est invalide."
 
-            // ]
-        )->validate();
+        // ]
+        //)->validate();
         // if ($validation->fails()) {
         //     dd('failed');
         // }
+
+        //2
+        $this->validate($request, [
+            'url' => ['required', 'url']
+        ]);
 
         //verify if url is already created it retourn it
         $record = Url::where('url', $url)->first();
